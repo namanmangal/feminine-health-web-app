27d69260f06c05045ca34219c004853b26ac0c04
Naman Mangal <mangal.naman.nm@gmail.com>
2024-03-13 10:45:55 -0700

    App Icon and Title

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ void main() {
     @override
     Widget build(BuildContext context) {
       return const MaterialApp(
+        title: 'Obulamu Choices',
         home: HomeScreen(),
       );
     }
